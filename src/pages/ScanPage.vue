1a4088d3a832d1383a2e8ba65024ac374b94656b
jahidanowar <milon5143@gmail.com>
2021-04-13 23:45:01 +0530

    UI Improvement

--- src/pages/ScanPage.vue
+++ src/pages/ScanPage.vue
@@ -33,7 +33,7 @@
     </qr-stream>
   </div>
   <div v-if="flash" class="text-center">
-      <button class="p-5 bg-white rounded-full" @click="flashActive =! flashActive" :class="{'bg-indigo-500 text-white':flashActive}">
+      <button class="p-5 bg-white rounded-full focus:outline-none" @click="flashActive =! flashActive" :class="{'bg-indigo-500 text-white':flashActive}">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
       </button>
   </div>
