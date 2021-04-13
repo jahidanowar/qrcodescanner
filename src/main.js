1ed6376dd16f5f0705a10175628221f6cb4fe392
jahidanowar <milon5143@gmail.com>
2021-04-13 20:51:22 +0530

    Meta Added

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import { createApp } from "vue";
+import QrReader from "vue3-qr-reader";
+import { registerSW } from "virtual:pwa-register";
+
 import App from "./App.vue";
-import router from "./router"
+import router from "./router";
 import "./assets/css/index.css";
 
-import QrReader from 'vue3-qr-reader';
-import { registerSW } from 'virtual:pwa-register'
-const updateSW = registerSW({
-    onNeedRefresh() {
-      // show a prompt to user
-    },
-    onOfflineReady() {
-      // show a ready to work offline to user
-    },
-  })
-
+//registerSW() registers service worker to the app
+registerSW();
 
 createApp(App).use(router).use(QrReader).mount("#app");
