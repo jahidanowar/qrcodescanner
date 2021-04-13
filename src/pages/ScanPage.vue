0ee0eeb25bc20a795a05e9021464fab164dd9418
jahidanowar <milon5143@gmail.com>
2021-04-13 14:16:24 +0530

    Scan Working

--- src/pages/ScanPage.vue
+++ src/pages/ScanPage.vue
@@ -1,22 +1,21 @@
 <template>
   <h1 class="text-center text-4xl font-bold">Scan your qrcode</h1>
-  <qr-stream
-    @decode="onDecode"
-  >
-  </qr-stream>
-  {{data}}
+  <div class="w-full md:w-96 h-auto overflow-hidden rounded-lg mx-auto my-10">
+    <qr-stream @decode="onDecode"> </qr-stream>
+  </div>
+  {{ data }}
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      data: null
-    }
+  data() {
+    return {
+      data: null,
+    };
   },
   methods: {
     onDecode(data) {
-      this.data = data
+      this.data = data;
       console.log(data);
     },
   },
