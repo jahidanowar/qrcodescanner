680b0516a1826b6e2957a30cba7d405ac76a8a22
jahidanowar <milon5143@gmail.com>
2021-04-14 10:27:19 +0530

    Copy functionn added in history page

--- src/pages/HistoryPage.vue
+++ src/pages/HistoryPage.vue
@@ -8,36 +8,65 @@
     >
       <p>{{ scan.content }}</p>
       <p>{{ new Date(scan.time).toDateString() }}</p>
-      <button class="bg-red-400 text-white p-2 rounded-full" @click="remove(i)">
-        <svg
-          class="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
+      <div class="inline-flex">
+        <button
+          class="bg-indigo-500 text-white p-2 rounded-full mr-2"
+          @click="copy(scan.content)"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-          ></path>
-        </svg>
-      </button>
+          <svg
+            class="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+            ></path>
+          </svg>
+        </button>
+        <button
+          class="bg-red-400 text-white p-2 rounded-full"
+          @click="remove(i)"
+        >
+          <svg
+            class="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            ></path>
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
   <div v-else class="container text-center">
     <p class="text-xl">Scan data not found 🤥</p>
     <p>
       Start scanning now 👉
-      <router-link :to="{ name: 'Home' }" class="text-indigo-500 underline">Scan Qr Code</router-link>
+      <router-link :to="{ name: 'Home' }" class="text-indigo-500 underline"
+        >Scan Qr Code</router-link
+      >
     </p>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import qrMixin from "./../mixins/qrMixin";
+
 export default {
+  mixins: [qrMixin],
   computed: {
     ...mapGetters(["getQrData"]),
   },
@@ -51,6 +80,15 @@ export default {
     },
     remove(index) {
       this.$store.dispatch("removeData", index);
+    },
+    copy(content) {
+      this.copyText(content)
+        .then((done) => {
+          alert("Copied");
+        })
+        .catch((e) => {
+          lert("Unable to copy");
+        });
     },
   },
   mounted() {
